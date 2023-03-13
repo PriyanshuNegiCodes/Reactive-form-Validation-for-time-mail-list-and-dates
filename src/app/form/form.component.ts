2d295f3110474b22204174a3b4c194498cde38ed
Priyanshu Negi <priyanshunegi2398@gmail.com>
2023-03-13 21:18:40 +0530

    changs done

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,5 +1,8 @@
 
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
+import { MatSnackBar } from '@angular/material/snack-bar';
+import { FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-form',
@@ -7,5 +10,45 @@ import { Component } from '@angular/core';
   styleUrls: ['./form.component.css']
 })
 export class FormComponent {
+  timeValues:string[]=[];
+  constructor (private fb: FormBuilder, private _snackBar: MatSnackBar){ }
+  
+  register=this.fb.group({
+    Title:['', Validators.required]
+  })
 
-}
+  get getTitleStatus(){
+    return this.register.get("Title")
+  }
+
+
+
+
+  
+  // timeValue():any{
+  //   for (let hour = 0; hour <= 23; hour++) {
+  //     for (let minute = 0; minute < 60; minute += 15) {
+  //       const hourString = hour.toString().padStart(2, '0');
+  //       const minuteString = minute.toString().padStart(2, '0');
+  //       const timeString = `${hourString}:${minuteString}`;
+  //       this.timeValues.push(timeString);
+  //     }
+  // }
+
+  //   check(){
+//     const time1 = '11:59';
+// const time2 = '12:00';
+
+// const date1 = new Date(`2000-01-01T${time1}:00`);
+// const date2 = new Date(`2000-01-01T${time2}:00`);
+
+// if (date1.getTime() > date2.getTime()) {
+//   console.log(`${time1} is later than ${time2}`);
+// } else if (date1.getTime() < date2.getTime()) {
+//   console.log(`${time1} is earlier than ${time2}`);
+// } else {
+//   console.log(`${time1} is the same as ${time2}`);
+// }
+//   }
+  }
+
